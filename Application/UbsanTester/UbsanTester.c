23c6f566665335b4338800de9f18ec5c7dabd06b
Anna Byvaltseva <a.a.byvalceva@gmail.com>
2024-02-21 10:44:11 +0300

    Fix bugs

--- Application/UbsanTester/UbsanTester.c
+++ Application/UbsanTester/UbsanTester.c
@@ -1,9 +1,8 @@
 #include "UbsanTester.h"
 
-__attribute__ ((nonnull)) 
-INT32
+__attribute__ ((nonnull)) INT32
 EFIAPI
-F1 (
+Nonnull1 (
   INT32  *Nonnull,
   INT32  n
   )
@@ -11,10 +10,9 @@ F1 (
   return *Nonnull;
 }
 
-__attribute__ ((nonnull)) 
-CHAR8
+__attribute__ ((nonnull)) CHAR8
 EFIAPI
-F2 (
+Nonnull2 (
   INT32  n,
   ...
   )
@@ -30,20 +28,18 @@ F2 (
   return res;
 }
 
-__attribute__ ((returns_nonnull)) 
-INT32 *
+__attribute__ ((returns_nonnull)) INT32 *
 EFIAPI
-F3 (
+Nonnull3 (
   INT32  *Nonnull
   )
 {
   return Nonnull;
 }
 
-__attribute__ ((returns_nonnull)) 
-INT32 *
+__attribute__ ((returns_nonnull)) INT32 *
 EFIAPI
-F4 (
+Nonnull4 (
   INT32  *Nonnull,
   INT32  n
   )
@@ -74,7 +70,7 @@ NonnullArg (
 
 INT32
 EFIAPI
-F6 (
+Bultin1 (
   INT32  n
   )
 {
@@ -84,7 +80,7 @@ F6 (
 
 INT64
 EFIAPI
-F7 (
+Bultin2 (
   INT64  n
   )
 {
@@ -94,7 +90,7 @@ F7 (
 
 INT32
 EFIAPI
-F8 (
+Bultin3 (
   INT32  n
   )
 {
@@ -104,7 +100,7 @@ F8 (
 
 INT64
 EFIAPI
-F9 (
+Bultin4 (
   INT64  n
   )
 {
@@ -122,19 +118,6 @@ GetInt (
   return p[i];
 }
 
-INT32
-EFIAPI
-GetIntFromMultidemArr (
-  INT32  i,
-  INT32  j,
-  INT32  k
-  )
-{
-  INT32  arr[2][3][4] = { };
-
-  return arr[i][j][k];
-}
-
 #ifdef TEST_BOUNDS
 VOID
 EFIAPI
@@ -142,18 +125,21 @@ BoundsCheck (
   VOID
   )
 {
-  INT32  Bar[2], i = 2, j = 3, k = 4;
+  INT32  Bar[2], i = 2;
+
+  // INT32  Arr[2][3][4] = { };
+  // INT32  Res, j = 3, k = 4;
 
   DEBUG ((DEBUG_INFO, "UBT: Start testing cases with bounds...\n\n\n"));
 
   GetInt (Bar, i);
   DEBUG ((DEBUG_WARN, "UBT: Index 2 is out of range for type \'INT32 *\' (aka 'int *')\n\n"));
-  GetIntFromMultidemArr (i, j - 1, k - 1);
-  DEBUG ((DEBUG_WARN, "UBT: Index 2 is out of range for type \'INT32[2][3][4]\' (aka 'int[2][3][4]')\n\n"));
-  GetIntFromMultidemArr (i - 1, j, k - 1);
-  DEBUG ((DEBUG_WARN, "UBT: Index 3 is out of range for type \'INT32[3][4]\' (aka 'int[3][4]')\n\n"));
-  GetIntFromMultidemArr (i - 1, j - 1, k);
-  DEBUG ((DEBUG_WARN, "UBT: Index 4 is out of range for type \'INT32[4]\' (aka 'int[4]')\n\n"));
+  // // Res = Arr[i][j - 1][k - 1];
+  // // DEBUG ((DEBUG_WARN, "UBT: Index 2 is out of range for type \'INT32[2][3][4]\' (aka 'int[2][3][4]')\n\n"));
+  // // Res = Arr[i - 1][j][k - 1];
+  // // DEBUG ((DEBUG_WARN, "UBT: Index 3 is out of range for type \'INT32[3][4]\' (aka 'int[3][4]')\n\n"));
+  // Res = Arr[i - 1][j - 1][k];
+  // DEBUG ((DEBUG_WARN, "UBT: Index 4 is out of range for type \'INT32[4]\' (aka 'int[4]')\n\n"));
 
   DEBUG ((DEBUG_INFO, "\nUBT: Checks with bounds are done...\n\n\n\n\n"));
 }
@@ -171,20 +157,20 @@ NonnullCheck (
 
   DEBUG ((DEBUG_INFO, "UBT: Start testing cases with nonnull atribute...\n\n\n"));
 
-  F1 (p, 8);
+  Nonnull1 (p, 8);
   DEBUG ((DEBUG_WARN, "UBT: Null pointer passed as argument 1, which is declared to never be null\n\n"));
   DEBUG ((DEBUG_WARN, "UBT: Load of null pointer of type 'INT32' (aka 'int')\n\n"));
-  F2 (8, p);
+  Nonnull2 (8, p);
   DEBUG ((DEBUG_WARN, "UBT: Null pointer passed as argument 2, which is declared to never be null\n\n"));
   DEBUG ((DEBUG_WARN, "UBT: Load of null pointer of type 'CHAR8' (aka 'char')\n\n"));
-  F3 (p);
+  Nonnull3 (p);
   DEBUG ((DEBUG_WARN, "UBT: Null pointer returned from function declared to never return null\n\n"));
-  F4 (p, 8);
+  Nonnull4 (p, 8);
   DEBUG ((DEBUG_WARN, "UBT: Null pointer returned from function declared to never return null\n\n"));
-  F4 (p, -8);
+  Nonnull4 (p, -8);
   DEBUG ((DEBUG_WARN, "UBT: Null pointer returned from function declared to never return null\n\n"));
 
-  NonnullReturn (NULL); // doesn't work
+  NonnullReturn (NULL);
   DEBUG ((DEBUG_WARN, "UBT: Null pointer returned from function declared to never return null\n\n"));
   NonnullArg (p);
   DEBUG ((DEBUG_WARN, "UBT: Null pointer passed as argument 1, which is declared to never be null\n\n"));
@@ -202,37 +188,19 @@ BuiltinCheck (
   )
 {
   DEBUG ((DEBUG_INFO, "UBT: Start testing cases with builtin...\n\n\n"));
-  F6 (0);
+  Bultin1 (0);
   DEBUG ((DEBUG_WARN, "UBT: Passing zero to ctz(), which is not a valid argument\n\n"));
-  F7 (0); // in ubsan in llvm also not unique message for ctzll
+  Bultin2 (0); // in ubsan in llvm also not unique message for ctzll
   DEBUG ((DEBUG_WARN, "UBT: Passing zero to ctz(), which is not a valid argument\n\n"));
-  F8 (0);
+  Bultin3 (0);
   DEBUG ((DEBUG_WARN, "UBT: Passing zero to clz(), which is not a valid argument\n\n"));
-  F9 (0);
+  Bultin4 (0);
   DEBUG ((DEBUG_WARN, "UBT: Passing zero to clz(), which is not a valid argument\n\n"));
 
   DEBUG ((DEBUG_INFO, "\nUBT: Checks with builtin are done...\n\n\n\n\n"));
 }
 
 #endif
-
-// #ifdef TEST_BOOLEAN
-// BOOLEAN
-// EFIAPI
-// BooleanCheck (
-//   VOID
-//   )
-// {
-//   UINT8    NotABool = 123;
-//   BOOLEAN  *P       = (BOOLEAN *)&NotABool;
-//   BOOLEAN  B        = *P;
-//   DEBUG ((DEBUG_INFO, "UBT: Start testing cases with boolean...\n\n\n"));
-//   DEBUG ((DEBUG_WARN, "UBT: Load of value 123, which is not a valid value for type 'BOOLEAN'\n\n"));
-//   DEBUG ((DEBUG_INFO, "\nUBT: Checks with boolean are done...\n\n\n\n\n"));
-
-//   return B;
-// }
-// #endif
 
 EFI_STATUS
 EFIAPI
@@ -241,10 +209,6 @@ UefiMain (
   IN EFI_SYSTEM_TABLE  *SystemTable
   )
 {
-  // TODO: delete me
-  // Print (L"\nUBT: Trap mode %d, %d...\n\n", (INT32) UBSAN_ALWAYS_FATAL, (INT32) FixedPcdGetBool (PcdOcGuardUBSanTrapMode));
-  // DEBUG ((DEBUG_INFO, "\nUBT: Trap mode %d, %d...\n\n", (INT32) UBSAN_ALWAYS_FATAL, (INT32) FixedPcdGetBool (PcdOcGuardUBSanTrapMode)));
-
  #ifdef TEST_ALIGNMENT
   AlignmentCheck ();
  #endif
@@ -260,13 +224,11 @@ UefiMain (
  #ifdef TEST_BUILTIN
   BuiltinCheck ();
  #endif
-
-  // #ifdef TEST_BOOLEAN
-  //   BooleanCheck (); // TODO: doesn't work
-  // #endif
-
  #ifdef TEST_IMPLICIT_CONVERSION
-  CheckConvertArithmeticsValue ();
+  ImplicitConversionCheck ();
+ #endif
+ #ifdef TEST_INTEGER
+  IntegerCheck ();
  #endif
   DEBUG ((DEBUG_INFO, "\nUBT: All tests are done...\n\n"));
 
