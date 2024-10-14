0ffb853b790f3b508c22b7eb7b88753c43263332
Anna Byvaltseva <a.a.byvalceva@gmail.com>
2024-10-14 16:19:45 +0300

    UbsanTester: Fixed some warnings

--- Application/UbsanTester/IntegerTest.c
+++ Application/UbsanTester/IntegerTest.c
@@ -13,31 +13,29 @@ ShiftOutOfBoundsCheck (
   INT32  I = 32;
 
   P >>= I;
-  DEBUG ((DEBUG_WARN, "\nUBT: Shift exponent 32 is too large for 32-bit type\n"));
+  DEBUG ((DEBUG_WARN, "\nUBT: Shift exponent %d is too large for %d-bit type\n", I, (sizeof (P) * 0x8)));
 
   P   = 1;
   I   = -1;
   P >>= I;
-  DEBUG ((DEBUG_WARN, "\nUBT: Shift exponent -1 is negative\n"));
+  DEBUG ((DEBUG_WARN, "\nUBT: Shift exponent %d is negative\n", I));
 
   P   = 1;
   I   = 32;
   P <<= I;
-  DEBUG ((DEBUG_WARN, "\nUBT: Shift exponent 32 is too large for 32-bit type\n"));
+  DEBUG ((DEBUG_WARN, "\nUBT: Shift exponent %d is too large for %d-bit type\n", I, (sizeof (P) * 0x8)));
 
   P   = 1;
   I   = -1;
   P <<= I;
-  DEBUG ((DEBUG_WARN, "\nUBT: Shift exponent -1 is negative\n"));
+  DEBUG ((DEBUG_WARN, "\nUBT: Shift exponent %d is negative\n", I));
 
   I = 31;
   I = 1 << I;
   DEBUG ((DEBUG_WARN, "\nUBT: Left shift of 1 by 31 places cannot be represented in type 'int'\n"));
 
-  INT32  Q = -1;
-
-  I   = 5;
-  Q <<= I;
+  I = 5;
+  I = (-1) << I;
   DEBUG ((DEBUG_WARN, "\nUBT: Left shift of negative value -1\n"));
 
   DEBUG ((DEBUG_WARN, "\n\nUBT: Checks with shift out of bounds are done...\n\n\n"));
@@ -70,6 +68,10 @@ AddOverflow (
   VOID
   )
 {
+  CONST CHAR16  UnknownType[] = L"unknown";
+  CONST CHAR16  Type32[]      = L"int";
+  CONST CHAR16  Type64[]      = L"INT64";
+
   DEBUG ((DEBUG_WARN, "\nUBT: Start testing cases with addition integer overflow...\n\n"));
 
   // These promote to 'int'.
@@ -79,12 +81,12 @@ AddOverflow (
   INT32  K1 = 0x12345678;
 
   K1 += 0x789abcde;
-  DEBUG ((DEBUG_WARN, "\nUBT: Signed integer overflow: 305419896 + 2023406814 cannot be represented in type 'int'\n"));
+  DEBUG ((DEBUG_WARN, "\nUBT: Signed integer overflow: 305419896 + 2023406814 cannot be represented in type '%s'\n", (sizeof (K1) == 4) ? Type32 : UnknownType));
 
   INT64  K2 = (INT64)8000000000000000000LL;
 
   K2 += (INT64)2000000000000000000LL;
-  DEBUG ((DEBUG_WARN, "\nUBT: Signed integer overflow: 8000000000000000000 + 2000000000000000000 cannot be represented in type\n"));
+  DEBUG ((DEBUG_WARN, "\nUBT: Signed integer overflow: 8000000000000000000 + 2000000000000000000 cannot be represented in type '%s'\n", (sizeof (K2) == 8) ? Type64 : UnknownType));
   DEBUG ((DEBUG_WARN, "\nUBT: Checks with addition integer overflow are done...\n\n\n"));
 }
 
@@ -94,6 +96,10 @@ SubOverflow (
   VOID
   )
 {
+  CONST CHAR16  UnknownType[] = L"unknown";
+  CONST CHAR16  Type32[]      = L"int";
+  CONST CHAR16  Type64[]      = L"INT64";
+
   DEBUG ((DEBUG_WARN, "\nUBT: Start testing cases with substraction integer overflow...\n\n"));
 
   // These promote to 'int'.
@@ -103,12 +109,12 @@ SubOverflow (
   INT32  K1 = (-2);
 
   K1 -= 0x7fffffff;
-  DEBUG ((DEBUG_WARN, "\nUBT: Signed integer overflow: -2 - 2147483647 cannot be represented in type 'int'\n"));
+  DEBUG ((DEBUG_WARN, "\nUBT: Signed integer overflow: -2 - 2147483647 cannot be represented in type '%s'\n", (sizeof (K1) == 4) ? Type32 : UnknownType));
 
   INT64  K2 = (INT64)(-8000000000000000000LL);
 
   K2 -= (INT64)(2000000000000000000LL);
-  DEBUG ((DEBUG_WARN, "\nUBT: Signed integer overflow: -8000000000000000000 - 2000000000000000000 cannot be represented in type\n"));
+  DEBUG ((DEBUG_WARN, "\nUBT: Signed integer overflow: -8000000000000000000 - 2000000000000000000 cannot be represented in type '%s'\n", (sizeof (K2) == 8) ? Type64 : UnknownType));
   DEBUG ((DEBUG_WARN, "\nUBT: Checks with substraction integer overflow are done...\n\n\n"));
 }
 
@@ -118,6 +124,9 @@ IncDecOverflow (
   VOID
   )
 {
+  CONST CHAR16  UnknownType[] = L"unknown";
+  CONST CHAR16  Type32[]      = L"INT32";
+
   DEBUG ((DEBUG_WARN, "\nUBT: Start testing cases with increment/decrement integer overflow...\n\n"));
 
   INT32  N1 = 0x7ffffffd, N2 = 0x7ffffffd;
@@ -129,14 +138,14 @@ IncDecOverflow (
   INT32  M1 = -N1 - 1, M2 = -N2 - 1;
 
   ++N1;
-  DEBUG ((DEBUG_WARN, "\nUBT: Signed integer overflow: 2147483647 + 1 cannot be represented in type 'int32' (aka 'int')\n"));
+  DEBUG ((DEBUG_WARN, "\nUBT: Signed integer overflow: 2147483647 + 1 cannot be represented in type '%s'\n", (sizeof (N1) == 4) ? Type32 : UnknownType));
   N2++;
-  DEBUG ((DEBUG_WARN, "\nUBT: Signed integer overflow: 2147483647 + 1 cannot be represented in type 'int32' (aka 'int')\n"));
+  DEBUG ((DEBUG_WARN, "\nUBT: Signed integer overflow: 2147483647 + 1 cannot be represented in type '%s'\n", (sizeof (N2) == 4) ? Type32 : UnknownType));
 
   --M1;
-  DEBUG ((DEBUG_WARN, "\nUBT: Signed integer overflow: -2147483648 - 1 cannot be represented in type 'int32' (aka 'int')\n"));
+  DEBUG ((DEBUG_WARN, "\nUBT: Signed integer overflow: -2147483648 - 1 cannot be represented in type '%s'\n", (sizeof (M1) == 4) ? Type32 : UnknownType));
   M2--;
-  DEBUG ((DEBUG_WARN, "\nUBT: Signed integer overflow: -2147483648 - 1 cannot be represented in type 'int32' (aka 'int')\n"));
+  DEBUG ((DEBUG_WARN, "\nUBT: Signed integer overflow: -2147483648 - 1 cannot be represented in type '%s'\n", (sizeof (M2) == 4) ? Type32 : UnknownType));
 
   DEBUG ((DEBUG_WARN, "\nUBT: Checks with increment/decrement integer overflow are done...\n\n\n"));
 }
@@ -170,6 +179,10 @@ UnsignedAddOverflow (
   VOID
   )
 {
+  CONST CHAR16  UnknownType[] = L"unknown";
+  CONST CHAR16  Type32[]      = L"unsigned int";
+  CONST CHAR16  Type64[]      = L"UINT64";
+
   DEBUG ((DEBUG_WARN, "\nUBT: Start testing cases with unsigned addition integer overflow...\n\n"));
 
   // These promote to 'int'.
@@ -179,12 +192,12 @@ UnsignedAddOverflow (
   UINT32  K1 = 0x87654321;
 
   K1 += 0xedcba987;
-  DEBUG ((DEBUG_WARN, "\nUBT: Unsigned integer overflow: 2271560481 + 3989547399 cannot be represented in type 'unsigned int'\n"));
+  DEBUG ((DEBUG_WARN, "\nUBT: Unsigned integer overflow: 2271560481 + 3989547399 cannot be represented in type '%s'\n", (sizeof (K1) == 4) ? Type32 : UnknownType));
 
   UINT64  K2 = (UINT64)10000000000000000000ULL;
 
   K2 += (UINT64)9000000000000000000ULL;
-  DEBUG ((DEBUG_WARN, "\nUBT: Unsigned integer overflow: 10000000000000000000 + 9000000000000000000 cannot be represented in type\n"));
+  DEBUG ((DEBUG_WARN, "\nUBT: Unsigned integer overflow: 10000000000000000000 + 9000000000000000000 cannot be represented in type '%s'\n", (sizeof (K2) == 8) ? Type64 : UnknownType));
   DEBUG ((DEBUG_WARN, "\nUBT: Checks with unsigned addition integer overflow are done...\n\n\n"));
 }
 
@@ -194,6 +207,10 @@ UnsignedSubOverflow (
   VOID
   )
 {
+  CONST CHAR16  UnknownType[] = L"unknown";
+  CONST CHAR16  Type32[]      = L"UINT32";
+  CONST CHAR16  Type64[]      = L"UINT64";
+
   DEBUG ((DEBUG_WARN, "\nUBT: Start testing cases with unsigned substraction integer overflow...\n\n"));
 
   // These promote to 'int'.
@@ -203,12 +220,12 @@ UnsignedSubOverflow (
   UINT32  K1 = 1;
 
   K1 -= 2;
-  DEBUG ((DEBUG_WARN, "\nUBT: Unsigned integer overflow: 1 - 2 cannot be represented in type\n"));
+  DEBUG ((DEBUG_WARN, "\nUBT: Unsigned integer overflow: 1 - 2 cannot be represented in type '%s'\n", (sizeof (K1) == 4) ? Type32 : UnknownType));
 
   UINT64  K2 = (UINT64)(8000000000000000000ULL);
 
   K2 -= (UINT64)(9000000000000000000ULL);
-  DEBUG ((DEBUG_WARN, "\nUBT: Unsigned integer overflow: 8000000000000000000 - 9000000000000000000 cannot be represented in type\n"));
+  DEBUG ((DEBUG_WARN, "\nUBT: Unsigned integer overflow: 8000000000000000000 - 9000000000000000000 cannot be represented in type '%s'\n", (sizeof (K2) == 8) ? Type64 : UnknownType));
   DEBUG ((DEBUG_WARN, "\nUBT: Checks with unsigned substraction integer overflow are done...\n\n\n"));
 }
 
@@ -218,6 +235,9 @@ UnsignedIncDecOverflow (
   VOID
   )
 {
+  CONST CHAR16  UnknownType[] = L"unknown";
+  CONST CHAR16  Type32[]      = L"UINT32";
+
   DEBUG ((DEBUG_WARN, "\nUBT: Start testing cases with unsigned increment/decrement integer overflow...\n\n"));
 
   UINT32  N1 = 0xfffffffd, N2 = 0xfffffffd;
@@ -229,14 +249,14 @@ UnsignedIncDecOverflow (
   UINT32  M1 = 0, M2 = 0;
 
   ++N1;
-  DEBUG ((DEBUG_WARN, "\nUBT: Unsigned integer overflow: 4294967295 + 1 cannot be represented in type 'UINT32' (aka 'unsigned int')\n"));
+  DEBUG ((DEBUG_WARN, "\nUBT: Unsigned integer overflow: 4294967295 + 1 cannot be represented in type '%s'\n", (sizeof (N1) == 4) ? Type32 : UnknownType));
   N2++;
-  DEBUG ((DEBUG_WARN, "\nUBT: Unsigned integer overflow: 4294967295 + 1 cannot be represented in type 'UINT32' (aka 'unsigned int')\n"));
+  DEBUG ((DEBUG_WARN, "\nUBT: Unsigned integer overflow: 4294967295 + 1 cannot be represented in type '%s'\n", (sizeof (N2) == 4) ? Type32 : UnknownType));
 
   --M1;
-  DEBUG ((DEBUG_WARN, "\nUBT: Unsigned integer overflow: 0 - 1 cannot be represented in type 'UINT32' (aka 'unsigned int')\n"));
+  DEBUG ((DEBUG_WARN, "\nUBT: Unsigned integer overflow: 0 - 1 cannot be represented in type '%s'\n", (sizeof (M1) == 4) ? Type32 : UnknownType));
   M2--;
-  DEBUG ((DEBUG_WARN, "\nUBT: Unsigned integer overflow: 0 - 1 cannot be represented in type 'UINT32' (aka 'unsigned int')\n"));
+  DEBUG ((DEBUG_WARN, "\nUBT: Unsigned integer overflow: 0 - 1 cannot be represented in type '%s'\n", (sizeof (M2) == 4) ? Type32 : UnknownType));
 
   DEBUG ((DEBUG_WARN, "\nUBT: Checks with unsigned increment/decrement integer overflow are done...\n\n\n"));
 }
